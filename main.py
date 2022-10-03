141b52fbd2cc205f5c3a8bfd3933642aac608459
andrewwer416 <awernersbach0361@floridapoly.edu>
2022-10-02 23:03:14 -0400

    Basic API to pull from query

---
+++ main.py
@@ -0,0 +1,11 @@
+import API_client
+import data_processing
+
+def main():
+    df_test = data_processing.read()
+    data_processing.preprocess(df_test)
+
+
+if __name__ == "__main__":
+    main()
+
